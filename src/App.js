0da4d5fc0c0138476a7d69aca8733d0b276531a8
Natasha Andreeva <tinskimmings@gmail.com>
2018-01-05 22:53:05 +0300

    Fetch CND-USD price from coinmarketcap

--- src/App.js
+++ src/App.js
@@ -1,18 +1,37 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './logo.jpg';
 import './App.css';
 
+
 class App extends Component {
+  constructor() {
+      super();
+      this.state = {
+          eth_price: '',
+      };
+  }
+
+  componentDidMount() {
+    fetch('https://api.coinmarketcap.com/v1/ticker/cindicator/', {header: {'Access-Control-Allow-Origin': '*'}})
+    .then((response) => response.json())
+    .then((data) => {
+        console.log(data);
+        let price = data[0]['price_usd'];
+        console.log(price);
+        this.setState({eth_price: price});
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">{this.state.eth_price}</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
