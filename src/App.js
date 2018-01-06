6cdfa027ebc3d318f1312739a4955bf6114ee601
Natasha Andreeva <tinskimmings@gmail.com>
2018-01-06 17:21:32 +0300

    Display 24h change

--- src/App.js
+++ src/App.js
@@ -7,43 +7,56 @@ class App extends Component {
   constructor() {
       super();
       this.state = {
-          usd_price: '',
-          btc_price: '',
-          eth_price: ''
+        usd_price: '',
+        btc_price: '',
+        eth_price: '',
+        prices_loaded: false,
+        change: '',
+        change_color: 'white'
       };
 
       this.updatePrice = this.updatePrice.bind(this);
   }
 
-  updatePrice() {
-    fetch('https://api.coinmarketcap.com/v1/ticker/cindicator/', {header: {'Access-Control-Allow-Origin': '*'}})
+  async updatePrice() {
+    await fetch('https://api.coinmarketcap.com/v1/ticker/cindicator/', {header: {'Access-Control-Allow-Origin': '*'}})
     .then((response) => response.json())
     .then((data) => {
         //console.log(data);
         let usd = data[0]['price_usd'];
         let btc = data[0]['price_btc'];
+        let change = data[0]['percent_change_24h'];
         let mBTC = btc * 1000;
         mBTC = mBTC.toFixed(6);
         //console.log(usd);
-        this.setState({usd_price: usd, btc_price: mBTC});
+
+        if ( change >= 0 ) {
+          this.setState({change_color: 'green'});
+        } else {
+          this.setState({change_color: 'red'});
+        };
+
+        this.setState({usd_price: usd, btc_price: mBTC, change: change});
     })
     .catch((error) => {
       console.error(error);
     });
 
-    fetch('https://api.coinmarketcap.com/v1/ticker/ethereum/', {header: {'Access-Control-Allow-Origin': '*'}})
+    await fetch('https://api.coinmarketcap.com/v1/ticker/ethereum/', {header: {'Access-Control-Allow-Origin': '*'}})
     .then((response) => response.json())
     .then((data) => {
         let usd = data[0]['price_usd'];
         console.log(usd);
-        let eth = parseFloat(this.state.usd_price) / usd;
-        let finney = eth.toFixed(9) * 1000;
+        let eth = parseFloat(this.state.usd_price) / usd * 1000;
+        let finney = eth.toFixed(6);
         console.log(finney);
         this.setState({eth_price: finney});
     })
     .catch((error) => {
       console.error(error);
     });
+
+    this.setState({prices_loaded: true});
   }
 
   componentDidMount() {
@@ -60,9 +73,15 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">{this.state.usd_price} USD</h1>
-          <h1 className="App-title">{this.state.btc_price} mBTC</h1>
-          <h1 className="App-title">{this.state.eth_price} mETH</h1>
+          {
+            this.state.prices_loaded ? (
+              <div>
+                <h1 className="App-title">$ {this.state.usd_price} <span style={{color: this.state.change_color}}>{this.state.change}%</span></h1>
+                <h1 className="App-title">{this.state.btc_price} mBTC</h1>
+                <h1 className="App-title">{this.state.eth_price} mETH</h1> 
+              </div>
+            ) : null
+          }
         </header>
       </div>
     );
