06fa41b33b114b5647acd8a73533732d070cbe93
Natasha Andreeva <tinskimmings@gmail.com>
2019-07-03 18:24:44 +0300

    Switch from CND price to OKR countdown
    Now we show how many days there is until December 9 and we have also updated the styles

--- src/App.js
+++ src/App.js
@@ -4,73 +4,30 @@ import './App.css';
 
 
 class App extends Component {
+
   constructor() {
       super();
       this.state = {
-        usd_price: '',
-        btc_price: '',
-        eth_price: '',
-        prices_loaded: false,
+        prices_loaded: true,
         change: '',
         change_color: 'white',
-        change_1h: '',
-        change_1h_color: 'white'
+        countdown: ''
       };
 
-      this.updatePrice = this.updatePrice.bind(this);
+      this.updateTime = this.updateTime.bind(this);
   }
 
-  async updatePrice() {
-    await fetch('https://api.coinmarketcap.com/v1/ticker/cindicator/', {header: {'Access-Control-Allow-Origin': '*'}})
-    .then((response) => response.json())
-    .then((data) => {
-        //console.log(data);
-        let usd = data[0]['price_usd'];
-        let btc = data[0]['price_btc'];
-        let change = data[0]['percent_change_24h'];
-        let change_1h = data[0]['percent_change_1h'];
-        let mBTC = btc * 1000;
-        mBTC = mBTC.toFixed(6);
-        //console.log(usd);
-
-        if ( change >= 0 ) {
-          this.setState({change_color: 'green'});
-        } else {
-          this.setState({change_color: 'red'});
-        };
-
-        if ( change_1h >= 0 ) {
-          this.setState({change_1h_color: 'green'});
-        } else {
-          this.setState({change_1h_color: 'red'});
-        };
-
-        this.setState({usd_price: usd, btc_price: mBTC, change: change, change_1h: change_1h});
-    })
-    .catch((error) => {
-      console.error(error);
-    });
-
-    await fetch('https://api.coinmarketcap.com/v1/ticker/ethereum/', {header: {'Access-Control-Allow-Origin': '*'}})
-    .then((response) => response.json())
-    .then((data) => {
-        let usd = data[0]['price_usd'];
-        console.log(usd);
-        let eth = parseFloat(this.state.usd_price) / usd * 1000;
-        let finney = eth.toFixed(6);
-        console.log(finney);
-        this.setState({eth_price: finney});
-    })
-    .catch((error) => {
-      console.error(error);
-    });
-
-    this.setState({prices_loaded: true});
+  updateTime() {
+    let countDownDate = new Date("Dec 9, 2019 03:00:00 UTC").getTime()
+    let today = new Date().getTime();
+    let distance = countDownDate - today;
+    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    this.setState({countdown: days});
   }
 
   componentDidMount() {
-    this.updatePrice();
-    this.interval = setInterval(() => this.updatePrice(), 60000);
+    this.updateTime();
+    this.interval = setInterval(() => this.updateTime(), 60000);
   }
 
   componentWillUnmount() {
@@ -85,13 +42,9 @@ class App extends Component {
               <div>
                 {/*<img src={logo} className="App-logo" alt="logo" />*/}
                 <div className="prices">
-                  <div className="usd">${this.state.usd_price} </div>
-                  <div className="change"><span style={{color: this.state.change_1h_color}}>{this.state.change_1h}%</span> in 1h || <span style={{color: this.state.change_color}}>{this.state.change}%</span> in 24h</div>
+                  <div className="usd">{this.state.countdown}</div>
+                  {/*<div className="change">До конца OKR осталось <span style={{color: this.state.change_color}}>{this.state.change}%</span> in 24h</div>*/}
                   <br /> <br />
-                  <div className="prices.additional">
-                    <div className="btc">{this.state.btc_price} mBTC</div>
-                    <div className="btc">{this.state.eth_price} mETH</div>
-                  </div> 
                 </div>
               </div>
             ) : null
